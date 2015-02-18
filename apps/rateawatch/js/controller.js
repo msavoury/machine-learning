51aabba60abc68d61330ffcedbda1b65e79c0c79
Marcos Savoury <marcossavoury@hotmail.com>
2015-02-17 23:12:04 -0500

    Basic app

--- apps/rateawatch/js/controller.js
+++ apps/rateawatch/js/controller.js
@@ -3,8 +3,40 @@ var watchApp = angular.module('watchApp', [], function($interpolateProvider) {
 	    $interpolateProvider.endSymbol(']]');
 });
 
+watchApp.factory('watchService', function($http) {
+	var svc = {};
+	svc.watches = {};
+	svc.getWatches = function() {
+		var promise = $http.get('/static-data/watches.json').then(function(response){
+			return response.data;
+		});
+		return promise;
+	};
 
-watchApp.controller('watchAppCtrl', ['$scope', function ($scope) {
-    $scope.dog = {name: 'poodler'};
+	return svc;
+
+});
+
+watchApp.controller('watchAppCtrl', ['$scope', 'watchService', function ($scope, watchService) {
+	$scope.watches = [];
+	$scope.currentWatch = function(){return{}};
+	$scope.currentWatchIndex = 0;
+
+	watchService.getWatches().then(function(d){
+		$scope.watches = d;
+		$scope.currentWatch = function() {return $scope.watches[$scope.currentWatchIndex]};
+	}); 
+
+	$scope.nextWatch = function() {
+		if ($scope.currentWatchIndex <= $scope.watches.length - 2) {
+			$scope.currentWatchIndex++;
+		}
+	}
+	
+	$scope.previousWatch = function() {
+		if ($scope.currentWatchIndex > 0) {
+			$scope.currentWatchIndex--;
+		}
+	}
 }]);
 
--- apps/watchapp/controller.js
+++
@@ -1,4 +0,0 @@
-var app = angular.module('app', []);
-app.controller('appCtrl', ['$scope', function ($scope) {
-    $scope.dog = {name: 'poodler'};
-}]);
