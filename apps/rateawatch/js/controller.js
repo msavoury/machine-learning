34ed93248020680f8372e42fbe4abae0278d23c9
Marcos Savoury <marcossavoury@hotmail.com>
2015-02-18 18:33:28 -0500

    Adding server side response

--- apps/rateawatch/js/controller.js
+++ apps/rateawatch/js/controller.js
@@ -1,4 +1,4 @@
-var watchApp = angular.module('watchApp', [], function($interpolateProvider) {
+var watchApp = angular.module('watchApp', ['ngAnimate'], function($interpolateProvider) {
 	    $interpolateProvider.startSymbol('[[');
 	    $interpolateProvider.endSymbol(']]');
 });
@@ -12,34 +12,59 @@ watchApp.factory('watchService', function($http) {
 		});
 		return promise;
 	};
-
 	return svc;
-
 });
 
-watchApp.controller('watchAppCtrl', ['$scope', 'watchService', function ($scope, watchService) {
+watchApp.factory('postService', function($http) {
+	var svc = {};
+	svc.postRatings = function(postObject) {
+		var promise = $http.post('/api/rating', postObject).then(function(response) {
+			return response.data;	
+		})	
+		return promise;
+	};
+	
+	return svc;
+});
+
+watchApp.controller('watchAppCtrl', ['$scope', 'watchService','postService' , function ($scope, watchService, postService) {
 	$scope.watches = [];
-	$scope.currentWatch = function(){return{}};
+	$scope.dummyWatch = { rating: 0, imageLink: ''};
+	$scope.currentWatch = function(){return $scope.dummyWatch};
 	$scope.currentWatchIndex = 0;
 	$scope.ratingOptions = [1,2,3,4,5];
+	$scope.username = "";
+	$scope.response = "repsonse";
 
 	watchService.getWatches().then(function(d){
 		$scope.watches = d;
-		$scope.currentWatch = function() {return $scope.watches[$scope.currentWatchIndex]};
+		$scope.currentWatch = function() {
+			if ($scope.currentWatchIndex >= $scope.watches.length) return $scope.dummyWatch;
+			return $scope.watches[$scope.currentWatchIndex]
+		};
 	}); 
 
+	$scope.postRatings = function() {
+		postObject = {};
+		postObject.username = $scope.username;
+		postObject.ratings = $scope.watches;
+		postService.postRatings(postObject).then(function(response){ 
+			console.log("response is here");
+			console.log(response);
+			$scope.response = response;
+		});
+	};
+	
 	$scope.setWatchRating = function(rating) {
 		$scope.currentWatch().rating = rating;
 	};
 
 	$scope.currentWatchMatchesRating = function(i) {
 		return $scope.currentWatch().rating == i;
-	}
+	};
 
 	$scope.nextWatch = function() {
-		if ($scope.currentWatchIndex <= $scope.watches.length - 2) {
-			$scope.currentWatchIndex++;
-		}
+		$scope.currentWatchIndex++;
 	}
 	
 	$scope.previousWatch = function() {
--- apps/rateawatch/rateawatch.py
+++ apps/rateawatch/rateawatch.py
@@ -2,6 +2,7 @@ import os
 import webapp2
 import jinja2
 import webapp2
+import json
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
@@ -17,7 +18,22 @@ class MainPage(webapp2.RequestHandler):
         template_value = { 'value': 8}
         self.response.write(template.render(template_value))
 
+class RatingPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template_value = { 'value': 8}
+        self.response.write('dammit')
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template_value = { 'value': 8}
+        json_data = json.loads(self.request.body)
+        json_data['stuff'] = "marcos"
+        self.response.write(json.dumps(json_data))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/api/rating', RatingPage),
 ], debug=True)
